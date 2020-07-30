1d608195ae5a3c47df272badbe97195f6cc66e8d
bhavanakota <46168256+bhavanakota@users.noreply.github.com>
2020-07-29 22:28:55 -0500

    Update Account.java

--- Lab4/src/Account.java
+++ Lab4/src/Account.java
@@ -1,8 +1,8 @@
 /* Exercise 2
  * Create a new class called Account with a main method that contains the following:
- * 			A static variable called numAccounts, initialized to 0.
- * 			A constructor method that will add 1 to the numAccounts variable each time a new
- * 			Account object is created.
+ * A static variable called numAccounts, initialized to 0.
+ * A constructor method that will add 1 to the numAccounts variable each time a new
+ * Account object is created.
  * A static method called getNumAccounts(). It should return numAccounts.
  * Test the functionality in the main method of Account by creating a few Account objects,
  *  then print out the number of accounts.
